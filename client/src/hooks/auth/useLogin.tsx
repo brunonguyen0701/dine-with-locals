9b94fae559c1f17b360217d61c1380066e72840e
brunonguyen0701 <bruno.nguyen0701@gmail.com>
2025-08-13 22:27:07 -0400

    log VITE_API_BASE_URL

--- client/src/hooks/auth/useLogin.tsx
+++ client/src/hooks/auth/useLogin.tsx
@@ -6,6 +6,7 @@ import { jwtDecode } from 'jwt-decode';
 import { getUserById } from '../../services/UserService';
 import { useUserContext } from '../useUserContext';
 import { AuthenticatedUser } from '../../../../shared/types/User';
+import { i } from 'node_modules/react-router/dist/development/lib-C1JSsICm.d.mts';
 // import BASE_URL from '../../../../shared/constants/constants';
 
 export const useLogin = () => {
@@ -26,6 +27,8 @@ export const useLogin = () => {
         `${import.meta.env.VITE_API_BASE_URL}api/auth/login`,
         user,
       );
+
+      console.log("VITE_API_BASE_URL:" + import.meta.env.VITE_API_BASE_URL);
 
       const { token } = result.data;
 
